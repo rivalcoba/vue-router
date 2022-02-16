3cec0d0d0c6953f66eddee1e6a08ecb53239e82f
rivalcoba <ivan.rivalcoba@gmail.com>
2022-02-16 14:40:33 -0600

    Se agregaron archivos de trabajo

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <br>
-  <router-link to="/">Inicio</router-link> |
-  <router-link to="/about">Acerca de</router-link>
+<div id="nav">
+  <router-link to="/">Inicio</router-link>
+  <router-link to="/brazil">Brasil</router-link>
+  <router-link to="/panama">Panama</router-link>
+  <router-link to="/hawaii">Hawaii</router-link>
+  <router-link to="/jamaica">Jamaica</router-link>
+</div>
+<div class="container">
   <router-view></router-view>
+</div>
 </template>
-<script setup>
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,10 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "@/views/Home.vue";
 import About from "@/views/About.vue";
+import Brazil from "@/views/Brazil.vue";
+import Panama from "@/views/Panama.vue";
+import Hawaii from "@/views/Hawaii.vue";
+import Jamaica from "@/views/Jamaica.vue";
 
 const routes = [
   {
@@ -13,6 +17,26 @@ const routes = [
     path: "/about",
     name: "About",
     component: About,
+  },
+  {
+    path: "/brazil",
+    name: "Brazil",
+    component: Brazil,
+  },
+  {
+    path: "/panama",
+    name: "Panama",
+    component: Panama,
+  },
+  {
+    path: "/hawaii",
+    name: "Hawaii",
+    component: Hawaii,
+  },
+  {
+    path: "/jamaica",
+    name: "Jamaica",
+    component: Jamaica,
   },
 ];
 
